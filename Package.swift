6bbbd5a9eccb5c9e1784c5ba399ec112b16fed21
Riley Flynn <riley@rileyflynn.me>
2020-12-28 23:33:02 -0330

    Math time

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 let package = Package(
     name: "Calcbot",
     dependencies: [
-        .package(url: "https://github.com/Azoy/Sword", .branch("master"))
+        .package(url: "https://github.com/Azoy/Sword", .branch("master")),
+        .package(url: "https://github.com/soulverteam/SoulverCore", .branch("master"))
     ],
     targets: [
         .target(
             name: "Calcbot",
-            dependencies: ["Sword"]),
+            dependencies: ["Sword", "SoulverCore"]),
     ]
 )
--- Sources/Calcbot/main.swift
+++ Sources/Calcbot/main.swift
@@ -1,14 +1,17 @@
 import Foundation
 import Sword
+import SoulverCore
 
 let options = ShieldOptions(
-  prefixes: ["c!"]
+  prefixes: ["ca!"]
 )
 
 let bot = Shield(token: ProcessInfo.processInfo.environment["CALCBOT_TOKEN"]!, shieldOptions: options)
 
 bot.register("calc") {msg, args in
-    print(args)
+    let lineCollection = LineCollection(multiLineText: args.joined(separator: " "), customization: .standard)
+    lineCollection.evaluateAll()
+    msg.reply(with: (lineCollection.lines.map {"\($0.expression) = \($0.result!.stringValue)"}).joined(separator: "\n"))
 }
 
 bot.connect()
